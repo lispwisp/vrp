2cf8e23d929dd181c53b88a08ab9929a2bf05841
Brian <lispwisp@gmail.com>
2024-05-22 16:09:14 -0600

    turn off tweak if it does not improve cost

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 def euclidean_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
-def compute_savings(pickup_dropoff_points):
+def compute_savings(pickup_dropoff_points, tweak=True):
     n = len(pickup_dropoff_points)
     savings = []
     for i, (p1_pickup, p1_dropoff) in enumerate(pickup_dropoff_points):
@@ -23,16 +23,17 @@ def compute_savings(pickup_dropoff_points):
                 # junky in the sense of 'there are plenty of nearly identical pitstops nearby that other routes could take'
                 # we are trying to load balance the junk so we don't end up with a small number of great schedules and a bunch of bad ones
                 # this heuristic should reduce the number of drivers overall by reducing the pressure from junk
-                proximity_score = 0
-                for k, (p3_pickup, p3_dropoff) in enumerate(pickup_dropoff_points):
-                    if k != i and k != j:
-                        proximity_score += (euclidean_distance(p1_pickup, p3_pickup) +
-                                            euclidean_distance(p1_dropoff, p3_dropoff) +
-                                            euclidean_distance(p2_pickup, p3_pickup) +
-                                            euclidean_distance(p2_dropoff, p3_dropoff))
-                proximity_score /= (n - 2) * 4
-                weighted_savings_value = savings_value / proximity_score
-                savings.append((weighted_savings_value, i, j))
+                if tweak:
+                    proximity_score = 0
+                    for k, (p3_pickup, p3_dropoff) in enumerate(pickup_dropoff_points):
+                        if k != i and k != j:
+                            proximity_score += (euclidean_distance(p1_pickup, p3_pickup) +
+                                                euclidean_distance(p1_dropoff, p3_dropoff) +
+                                                euclidean_distance(p2_pickup, p3_pickup) +
+                                                euclidean_distance(p2_dropoff, p3_dropoff))
+                    proximity_score /= (n - 2) * 4
+                    savings_value /= proximity_score
+                savings.append((savings_value, i, j))
     return sorted(savings, reverse=True)
 
 def calculate_route_distance(route, loads):
@@ -48,21 +49,45 @@ def calculate_route_distance(route, loads):
 
 def tweaked_clarke_wright_savings_algorithm(loads, max_route_time=720):  # max_route_time in minutes (12 hours)
     pickup_dropoff_points = [(load['pickup'], load['dropoff']) for load in loads]
-    routes = [[i] for i in range(len(loads))]
-    savings = compute_savings(pickup_dropoff_points)
-    load_to_route = {i: route for i, route in enumerate(routes)}
-    for saving, i, j in savings:
-        if load_to_route[i] != load_to_route[j]:
-            route_i = load_to_route[i]
-            route_j = load_to_route[j]
-            combined_route = route_i + route_j
-            if calculate_route_distance(combined_route, loads) <= max_route_time:
-                routes.remove(route_i)
-                routes.remove(route_j)
-                routes.append(combined_route)
-                for load_index in combined_route:
-                    load_to_route[load_index] = combined_route
+    r = []
+    for tweak in [True, False]:
+        routes = [[i] for i in range(len(loads))]
+        savings = compute_savings(pickup_dropoff_points, tweak)
+        load_to_route = {i: route for i, route in enumerate(routes)}
+        for saving, i, j in savings:
+            if load_to_route[i] != load_to_route[j]:
+                route_i = load_to_route[i]
+                route_j = load_to_route[j]
+                combined_route = route_i + route_j
+                if calculate_route_distance(combined_route, loads) <= max_route_time:
+                    routes.remove(route_i)
+                    routes.remove(route_j)
+                    routes.append(combined_route)
+                    for load_index in combined_route:
+                        load_to_route[load_index] = combined_route
+        cost = calculate_total_cost(routes, loads)
+        r.append((cost, routes))
+    win = min(r, key=lambda x: x[0])
+    routes = win[1]
     return routes
+
+def calculate_total_cost(routes, loads):
+    total_driving_time = 0
+    number_of_drivers = len(routes)
+
+    for route in routes:
+        route_distance = 0
+        current_location = (0, 0)
+        for load_index in route:
+            pickup, dropoff = loads[load_index]['pickup'], loads[load_index]['dropoff']
+            route_distance += euclidean_distance(current_location, pickup)
+            route_distance += euclidean_distance(pickup, dropoff)
+            current_location = dropoff
+        route_distance += euclidean_distance(current_location, (0, 0))
+        total_driving_time += route_distance
+
+    total_cost = 500 * number_of_drivers + total_driving_time
+    return total_cost
 
 def read_loads_from_file(filename):
     loads = []
